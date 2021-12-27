93c87132f3e0bb802587da643d99394770399edc
Nicholas Long <nicholas.long@nrel.gov>
2021-12-27 16:36:08 -0700

    pull version from env var

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -4,16 +4,8 @@ IMAGETAG=skip
 if [ "${GITHUB_REF}" == "refs/heads/develop" ]; then
     IMAGETAG=develop
 elif [ "${GITHUB_REF}" == "refs/heads/main" ]; then
-    # Retrieve the version number from package.json - 9.5.0 has date with decimal separator, so grab first line withhead -n1
-    IMAGETAG=$( docker run -it energyplus:latest /bin/bash -c "EnergyPlus --version | grep -Po '\d{1,2}\.\d{1,2}\.\d{1,2}'" | head -n1 )
-    OUT=$?
-    if [ $OUT -eq 0 ]; then
-        IMAGETAG=$( echo $IMAGETAG | tr -d '\r' )
-        echo "Found EnergyPlus Version: $IMAGETAG"
-    else
-        echo "ERROR Trying to find EnergyPlus Version"
-        IMAGETAG=skip
-    fi
+    # grab the version from the build's env variables
+    IMAGETAG=${ENERGYPLUS_VERSION}
 fi
 
 if [ "${IMAGETAG}" != "skip" ]; then
@@ -25,7 +17,7 @@ if [ "${IMAGETAG}" != "skip" ]; then
     docker push nrel/energyplus:$IMAGETAG; (( exit_status = exit_status || $? ))
 
     if [ "${GITHUB_REF}" == "refs/heads/main" ]; then
-	# Deploy main as the latest.
+	    # Deploy main as the latest.
         docker push nrel/energyplus:latest; (( exit_status = exit_status || $? ))
     fi
 
