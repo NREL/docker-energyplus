d6ee470eb1fbe530cdd7f10dcdfb63d1b77e02ec
Alex Swindler <Alex.Swindler@nrel.gov>
2023-10-30 10:46:07 -0600

    Temporarily override publishing rules

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/env bash
 
-IMAGETAG=skip
-if [ "${GITHUB_REF}" == "refs/heads/develop" ]; then
-    IMAGETAG=develop
-elif [ "${GITHUB_REF}" == "refs/heads/main" ]; then
-    # grab the version from the build's env variables
-    IMAGETAG=${ENERGYPLUS_VERSION}
-fi
+IMAGETAG=9.6.0-python3.7
 
 if [ "${IMAGETAG}" != "skip" ]; then
     echo "Tagging image as $IMAGETAG"
 
     echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
     docker tag energyplus:latest nrel/energyplus:$IMAGETAG; (( exit_status = exit_status || $? ))
-    docker tag energyplus:latest nrel/energyplus:latest; (( exit_status = exit_status || $? ))
     docker push nrel/energyplus:$IMAGETAG; (( exit_status = exit_status || $? ))
 
     if [ "${GITHUB_REF}" == "refs/heads/main" ]; then
-	    # Deploy main as the latest.
+        # Deploy main as the latest.
         docker push nrel/energyplus:latest; (( exit_status = exit_status || $? ))
     fi
 
     exit $exit_status
 else
-    echo "Not on a deployable branch, this is a pull request or has been explicity skipped"
+    echo "Not on a deployable branch, this is a pull request or has been explicitly skipped"
 fi
 
 # Deploy the singularity image
